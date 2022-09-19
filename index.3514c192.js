423124736a1e952c8f6fffb47b32fa7fff6b4ce9
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-09-19 00:15:50 +0000

    Deploying to gh-pages from @ goodcat46/goit-js-hw-11@438a8b20c37e71b1edfa96f587f1ca54bdb64317 🚀

---
+++ index.3514c192.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slick-slider-container").slick({dots:!0,infinite:!0,arrows:!0,slidesToShow:1,slidesToScroll:1,prevArrow:'<button type="button" class="slick-prev">&#60;</button>',nextArrow:'<button type="button" class="slick-next">&#62;</button>'})}));
+//# sourceMappingURL=index.3514c192.js.map
